7de9291eb5a4e49cc047d7f8dc0aaf1fece4f9ff
George Higbie <gehigbie@gmail.com>
2016-10-27 18:12:51 -0400

    added fragment chooser page

---
+++ app/src/main/java/layout/ChooserPage.java
@@ -0,0 +1,24 @@
+package layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.geogehigbie.developerhealthplus.R;
+
+
+public class ChooserPage extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chooser_page, container, false);
+    }
+
+
+
+}
